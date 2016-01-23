23ed56c54e91c6688fd730c8728b09587cd61810
Daniel Lovasko <lovasko@freebsd.org>
2016-01-23 21:42:48 +0100

    all prototypes have only one line

--- src/m_array.h
+++ src/m_array.h
@@ -25,37 +25,21 @@ struct m_array {
 	double growth_factor;
 };
 
-int m_array_init(struct m_array* array,
-                 size_t initial_length,
-                 size_t object_size);
+int m_array_init(struct m_array* array, size_t initial_length, size_t object_size);
 int m_array_length(struct m_array* array, size_t* out_length);
 int m_array_growth_factor(struct m_array* array, double new_growth_factor);
 int m_array_resize(struct m_array* array, size_t new_length);
 int m_array_trim(struct m_array* array);
-
 int m_array_get(struct m_array* array, size_t index, void** out_data);
-int m_array_set(struct m_array* array,
-                size_t index,
-                size_t object_count,
-                void* data);
-
+int m_array_set(struct m_array* array, size_t index, size_t object_count, void* data);
 int m_array_append(struct m_array* array, size_t object_count, void* data);
 int m_array_prepend(struct m_array* array, size_t object_count, void* data);
-int m_array_insert(struct m_array* array,
-                   size_t index,
-                   size_t object_count,
-                   void* data);
-
+int m_array_insert(struct m_array* array, size_t index, size_t object_count, void* data);
 int m_array_remove(struct m_array* array, size_t index, size_t object_count);
 int m_array_remove_all(struct m_array* array);
-
 int m_array_map(struct m_array* array, void(*fn)(void*,void*), void* payload);
 int m_array_sort(struct m_array* array, int(*cmp_fn)(const void*,const void*));
-int m_array_search(struct m_array* array,
-                   void* key,
-                   int(*cmp_fn)(const void*,const void*),
-                   int is_sorted,
-                   size_t* out_index);
+int m_array_search(struct m_array* array, void* key, int(*cmp_fn)(const void*,const void*), int is_sorted, size_t* out_index);
 
 #endif
 
